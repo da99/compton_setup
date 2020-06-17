68963de04a5d44bfe84ec4a6fa027364cb86abee
da99 <sp.03+github.spam@miniuni.com>
2020-06-17 00:21:32 -0500

    Updated: compton -> picom

--- bin/public/start/_.sh
+++ bin/public/start/_.sh
@@ -2,10 +2,10 @@
 # === {{CMD}}
 start () {
   local +x CONF="main.conf"
-  if pgrep -f "compton .+/$CONF" &>/dev/null; then
-    echo "=== Compton already running." >&2
+  if pgrep -f "picom .+/$CONF" &>/dev/null; then
+    echo "=== picom already running." >&2
     return 0
   fi
 
-  exec compton --config "$THIS_DIR"/$CONF
+  exec picom --config "$THIS_DIR"/$CONF
 } # === end function
